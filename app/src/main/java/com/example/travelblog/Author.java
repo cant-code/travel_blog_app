77a40696197604faff65cb614f0cf2e8483b9af9
Ayush <ayushc13000@gmail.com>
2021-06-12 20:19:54 +0530

    Added List Adapter

--- app/src/main/java/com/example/travelblog/Author.java
+++ app/src/main/java/com/example/travelblog/Author.java
@@ -1,9 +1,25 @@
 package com.example.travelblog;
 
+import java.util.Objects;
+
 public class Author {
 
     private String name;
     private String avatar;
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) &&
+                Objects.equals(avatar, author.avatar);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, avatar);
+    }
 
     public String getName() {
         return this.name;
@@ -11,5 +27,9 @@ public class Author {
 
     public String getAvatar() {
         return this.avatar;
+    }
+
+    public String getAvatarUrl() {
+        return BlogHttpClient.BASE_URL + BlogHttpClient.PATH + getAvatar();
     }
 }
